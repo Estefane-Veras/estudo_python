ab0606450f9847134ffb4e5d116b8d07f1168d32
Estefane <estefane.infor2@gmail.com>
2021-05-30 20:11:10 -0300

    Alteração de soma_diferença_quadrada.py

--- soma_diferenca_quadrada.py
+++ soma_diferenca_quadrada.py
@@ -13,15 +13,32 @@ Encontre a diferença entre a soma dos quadrados dos primeiros cem números natu
 soma.
 '''
 
-soma_quadrados = 385
-quadrado_soma = 55
+# soma_quadrados = 385
+# quadrado_soma = 55
 
-for i in range(11, 101):
-	soma_quadrados = soma_quadrados + (i**2)
-	quadrado_soma = quadrado_soma + i
+# for i in range(11, 101):
+# 	soma_quadrados = soma_quadrados + (i**2)
+# 	quadrado_soma = quadrado_soma + i
 
-quadrado_soma = quadrado_soma**2
+# quadrado_soma = quadrado_soma**2
 
-diferenca = quadrado_soma - soma_quadrados
+# diferenca = quadrado_soma - soma_quadrados
+
+# print(diferenca)
+
+def soma_quadrada(num):
+	soma = 0
+	for x in range(1,num+1):
+		soma = soma + (x**2)
+	return soma
+
+def quadrado_soma(num):
+	quadrado = 0
+	for x in range(1,num + 1):
+		quadrado = quadrado + x
+	quadrado = quadrado**2
+	return quadrado
+
+diferenca = quadrado_soma(100) - soma_quadrada(100)
 
 print(diferenca)
